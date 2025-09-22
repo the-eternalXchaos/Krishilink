6ad0d6805de472bbaf35c0221e4a8e28ad0bde5f
Sandy99098 <waglesandeep8@gmail.com>
2025-09-22 13:46:09 +0545

    fix: add missing import for material_ui pop_up in AdminOrderController

--- lib/features/admin/controllers/admin_order_controller.dart
+++ lib/features/admin/controllers/admin_order_controller.dart
@@ -7,6 +7,7 @@ import 'package:krishi_link/core/utils/api_constants.dart';
 import 'package:krishi_link/features/admin/models/order_model.dart';
 import 'package:krishi_link/services/token_service.dart';
 import 'package:krishi_link/core/lottie/popup_service.dart';
+import 'package:krishi_link/src/core/components/material_ui/pop_up.dart';
 
 class AdminOrderController extends GetxController {
   final orders = <OrderModel>[].obs;
