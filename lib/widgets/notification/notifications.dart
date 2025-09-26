f784f19d4e0428fa6ef11aa15da4f7f7e40c4323
Sandy99098 <waglesandeep8@gmail.com>
2025-09-26 10:59:32 +0545

    chore: update lib/widgets/notification/notifications.dart

--- lib/widgets/notification/notifications.dart
+++ lib/widgets/notification/notifications.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:animate_do/animate_do.dart';
 import 'package:krishi_link/src/core/constants/app_spacing.dart';
 import 'package:krishi_link/core/lottie/popup_service.dart';
-import 'package:krishi_link/features/admin/models/notification_model.dart';
+import 'package:krishi_link/features/notification/model/notification_model.dart';
 import 'package:krishi_link/src/features/notification/presentation/controllers/notification_controller.dart';
 
 class NotificationsScreen extends StatelessWidget {
