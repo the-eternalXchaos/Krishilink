a311f68871159aa5c69216b10984dba5bc147f69
Sandy99098 <waglesandeep8@gmail.com>
2025-09-26 10:57:59 +0545

    chore: update lib/features/auth/controller/cart_controller.dart

--- lib/features/auth/controller/cart_controller.dart
+++ lib/features/auth/controller/cart_controller.dart
@@ -4,10 +4,9 @@ import 'package:dio/dio.dart';
 import 'package:get/get.dart' hide Response;
 import 'package:krishi_link/core/lottie/popup_service.dart';
 import 'package:krishi_link/core/utils/api_constants.dart';
-import 'package:krishi_link/features/admin/models/product_model.dart';
-import 'package:krishi_link/features/cart/models/cart_item.dart';
+import 'package:krishi_link/src/features/product/data/models/product_model.dart';
+import 'package:krishi_link/features/buyer/screens/cart_screen.dart';
 import 'package:krishi_link/src/core/networking/api_service.dart';
-import 'package:krishi_link/services/popup_service.dart';
 import 'package:krishi_link/src/features/auth/data/token_service.dart';
 import 'package:krishi_link/src/core/components/material_ui/pop_up.dart';
 
