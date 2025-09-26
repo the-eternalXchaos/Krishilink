2e2b1ed3540ba0e036238490efbd38b559faed80
Sandy99098 <waglesandeep8@gmail.com>
2025-09-26 10:58:21 +0545

    chore: update lib/features/product/bindings/product_detail_binding.dart

--- lib/features/product/bindings/product_detail_binding.dart
+++ lib/features/product/bindings/product_detail_binding.dart
@@ -5,7 +5,6 @@ import 'package:krishi_link/src/features/product/presentation/controllers/produc
 class ProductDetailBinding extends Bindings {
   @override
   void dependencies() {
-    // Lazy product controller (shared singleton) reused by detail pages
     Get.lazyPut<ProductController>(() => ProductController(), fenix: true);
   }
 }
