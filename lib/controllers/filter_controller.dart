662f504bdc8b37678d0b18c5eb55235c81cfee78
Sandy99098 <waglesandeep8@gmail.com>
2025-10-29 20:58:45 +0545

    chore: update lib/controllers/filter_controller.dart

--- lib/controllers/filter_controller.dart
+++
@@ -1,4 +0,0 @@
-// Legacy export shim. Keep this file so any imports of
-// `package:krishi_link/controllers/filter_controller.dart` resolve
-// to the feature-first implementation under `lib/src/...`.
-export 'package:krishi_link/src/features/product/presentation/controllers/filter_controller.dart';
