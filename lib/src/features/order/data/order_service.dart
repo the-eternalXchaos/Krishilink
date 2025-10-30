81cb83fadc11bc499531d864f184909331ff4fa4
Sandy99098 <waglesandeep8@gmail.com>
2025-10-30 14:08:23 +0545

    chore: update lib/src/features/order/data/order_service.dart

--- lib/src/features/order/data/order_service.dart
+++ lib/src/features/order/data/order_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart' hide Response, FormData, MultipartFile;
 import 'package:krishi_link/core/utils/api_constants.dart';
 import 'package:krishi_link/src/core/networking/api_service.dart';
@@ -7,15 +8,31 @@ import 'package:krishi_link/src/core/networking/api_service.dart';
 class OrderService {
   late final ApiService _api;
   OrderService({ApiService? api}) {
-    _api = api ?? (Get.isRegistered<ApiService>() ? Get.find<ApiService>() : ApiService());
+    _api =
+        api ??
+        (Get.isRegistered<ApiService>()
+            ? Get.find<ApiService>()
+            : ApiService());
   }
 
   // --- List endpoints ---
   Future<Response<dynamic>> getMyOrders() async {
-    return _api.dio.get(
+    final res = await _api.dio.get(
       ApiConstants.getMyOrdersEndpoint,
       options: Options(headers: {'accept': '*/*'}),
     );
+    // Debug: log response shape (truncate to avoid huge logs)
+    try {
+      final str = res.data.toString();
+      final preview = str.length > 1500 ? str.substring(0, 1500) + '…' : str;
+      // ignore: avoid_print
+      // Use debugPrint to split long strings safely
+      // printing a header for clarity
+      debugPrint('[Orders][getMyOrders] status: ${res.statusCode}');
+      debugPrint('[Orders][getMyOrders] body (preview):');
+      debugPrint(preview);
+    } catch (_) {}
+    return res;
   }
 
   Future<Response<dynamic>> getCustomerOrders() async {
@@ -27,10 +44,15 @@ class OrderService {
 
   // --- Order-level ---
   Future<Response<dynamic>> cancelOrder(String orderId) async {
-    return _api.dio.put(
+    final res = await _api.dio.put(
       '${ApiConstants.cancelOrderEndpoint}/$orderId',
       options: Options(headers: {'accept': '*/*'}),
     );
+    try {
+      debugPrint('[Orders][cancelOrder] orderId=$orderId status: ${res.statusCode}');
+      debugPrint('[Orders][cancelOrder] body: ${res.data}');
+    } catch (_) {}
+    return res;
   }
 
   // --- Item-level ---
@@ -70,5 +92,20 @@ class OrderService {
       '${ApiConstants.markAsDeliveryEndpoint}/$orderItemId',
       options: Options(headers: {'accept': '*/*'}),
     );
+  }
+
+  // --- Product details ---
+  Future<Response<dynamic>> getProductById(String productId) async {
+    try {
+      final res = await _api.dio.get(
+        '${ApiConstants.getProductByIdEndpoint}/$productId',
+        options: Options(headers: {'accept': '*/*'}),
+      );
+      debugPrint('[Orders][getProductById] productId=$productId status: ${res.statusCode}');
+      return res;
+    } catch (e) {
+      debugPrint('[Orders][getProductById] Error fetching product $productId: $e');
+      rethrow;
+    }
   }
 }
