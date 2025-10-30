6a367f3cfa30037a5a8ef43a9e33f078d5c8d0ac
Sandy99098 <waglesandeep8@gmail.com>
2025-10-30 14:08:25 +0545

    chore: update lib/src/features/payment/data/backend_payment_service.dart

--- lib/src/features/payment/data/backend_payment_service.dart
+++ lib/src/features/payment/data/backend_payment_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart' hide Response, FormData, MultipartFile;
 import 'package:krishi_link/core/utils/api_constants.dart';
 import 'package:krishi_link/src/core/networking/api_service.dart';
@@ -16,22 +17,59 @@ import 'package:krishi_link/src/core/networking/api_service.dart';
 class BackendPaymentService {
   late final ApiService _api;
   BackendPaymentService({ApiService? api}) {
-    _api = api ?? (Get.isRegistered<ApiService>() ? Get.find<ApiService>() : ApiService());
+    _api =
+        api ??
+        (Get.isRegistered<ApiService>()
+            ? Get.find<ApiService>()
+            : ApiService());
   }
 
   Future<Response<dynamic>> initiateEsewa({
     required String cartId,
     required double totalPayableAmount,
   }) async {
+    if (kDebugMode) {
+      debugPrint('[Payment][eSewa] Initiating payment');
+      debugPrint(
+        '[Payment][eSewa] Endpoint: ${ApiConstants.initiateEsewaPaymentEndpoint}',
+      );
+      debugPrint(
+        '[Payment][eSewa] Payload: {cartId: $cartId, totalPayableAmount: ${totalPayableAmount.toStringAsFixed(2)}}',
+      );
+    }
     final form = FormData.fromMap({
       'cartId': cartId,
       'totalPayableAmount': totalPayableAmount,
     });
-    return _api.dio.post(
-      ApiConstants.initiateEsewaPaymentEndpoint,
-      data: form,
-      options: Options(contentType: 'multipart/form-data'),
-    );
+    try {
+      final res = await _api.dio.post(
+        ApiConstants.initiateEsewaPaymentEndpoint,
+        data: form,
+        options: Options(
+          contentType: 'multipart/form-data',
+          headers: {'accept': '*/*'},
+        ),
+      );
+      if (kDebugMode) {
+        debugPrint(
+          '[Payment][eSewa] Status: ${res.statusCode?.toString() ?? 'null'}',
+        );
+        debugPrint('[Payment][eSewa] Response: ${res.data}');
+      }
+      return res;
+    } on DioException catch (e) {
+      if (kDebugMode) {
+        debugPrint('[Payment][eSewa][ERROR] Type: ${e.type}');
+        debugPrint('[Payment][eSewa][ERROR] Message: ${e.message ?? ''}');
+        debugPrint(
+          '[Payment][eSewa][ERROR] Status: ${e.response?.statusCode?.toString() ?? 'null'}',
+        );
+        debugPrint(
+          '[Payment][eSewa][ERROR] Data: ${e.response?.data?.toString() ?? 'null'}',
+        );
+      }
+      rethrow;
+    }
   }
 
   Future<Response<dynamic>> esewaSuccess({required String data}) async {
@@ -54,15 +92,48 @@ class BackendPaymentService {
     required String cartId,
     required double totalPayableAmount,
   }) async {
+    if (kDebugMode) {
+      debugPrint('[Payment][Khalti] Initiating payment');
+      debugPrint(
+        '[Payment][Khalti] Endpoint: ${ApiConstants.initiateKhaltiPaymentEndpoint}',
+      );
+      debugPrint(
+        '[Payment][Khalti] Payload: {cartId: $cartId, totalPayableAmount: ${totalPayableAmount.toStringAsFixed(2)}}',
+      );
+    }
     final form = FormData.fromMap({
       'cartId': cartId,
       'totalPayableAmount': totalPayableAmount,
     });
-    return _api.dio.post(
-      ApiConstants.initiateKhaltiPaymentEndpoint,
-      data: form,
-      options: Options(contentType: 'multipart/form-data'),
-    );
+    try {
+      final res = await _api.dio.post(
+        ApiConstants.initiateKhaltiPaymentEndpoint,
+        data: form,
+        options: Options(
+          contentType: 'multipart/form-data',
+          headers: {'accept': '*/*'},
+        ),
+      );
+      if (kDebugMode) {
+        debugPrint(
+          '[Payment][Khalti] Status: ${res.statusCode?.toString() ?? 'null'}',
+        );
+        debugPrint('[Payment][Khalti] Response: ${res.data}');
+      }
+      return res;
+    } on DioException catch (e) {
+      if (kDebugMode) {
+        debugPrint('[Payment][Khalti][ERROR] Type: ${e.type}');
+        debugPrint('[Payment][Khalti][ERROR] Message: ${e.message ?? ''}');
+        debugPrint(
+          '[Payment][Khalti][ERROR] Status: ${e.response?.statusCode?.toString() ?? 'null'}',
+        );
+        debugPrint(
+          '[Payment][Khalti][ERROR] Data: ${e.response?.data?.toString() ?? 'null'}',
+        );
+      }
+      rethrow;
+    }
   }
 
   Future<Response<dynamic>> khaltiResponse({
@@ -98,14 +169,47 @@ class BackendPaymentService {
     required String cartId,
     required double totalPayableAmount,
   }) async {
+    if (kDebugMode) {
+      debugPrint('[Payment][COD] Initiating COD');
+      debugPrint(
+        '[Payment][COD] Endpoint: ${ApiConstants.cashOnDeliveryEndpoint}',
+      );
+      debugPrint(
+        '[Payment][COD] Payload: {cartId: $cartId, totalPayableAmount: ${totalPayableAmount.toStringAsFixed(2)}}',
+      );
+    }
     final form = FormData.fromMap({
       'cartId': cartId,
       'totalPayableAmount': totalPayableAmount,
     });
-    return _api.dio.post(
-      ApiConstants.cashOnDeliveryEndpoint,
-      data: form,
-      options: Options(contentType: 'multipart/form-data'),
-    );
+    try {
+      final res = await _api.dio.post(
+        ApiConstants.cashOnDeliveryEndpoint,
+        data: form,
+        options: Options(
+          contentType: 'multipart/form-data',
+          headers: {'accept': '*/*'},
+        ),
+      );
+      if (kDebugMode) {
+        debugPrint(
+          '[Payment][COD] Status: ${res.statusCode?.toString() ?? 'null'}',
+        );
+        debugPrint('[Payment][COD] Response: ${res.data}');
+      }
+      return res;
+    } on DioException catch (e) {
+      if (kDebugMode) {
+        debugPrint('[Payment][COD][ERROR] Type: ${e.type}');
+        debugPrint('[Payment][COD][ERROR] Message: ${e.message ?? ''}');
+        debugPrint(
+          '[Payment][COD][ERROR] Status: ${e.response?.statusCode?.toString() ?? 'null'}',
+        );
+        debugPrint(
+          '[Payment][COD][ERROR] Data: ${e.response?.data?.toString() ?? 'null'}',
+        );
+      }
+      rethrow;
+    }
   }
 }
